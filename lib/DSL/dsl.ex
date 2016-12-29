cd8dbf5e2d5ed01af8870b00c26be21a59bf5df9
Vadim Shashin <enapiuz@gmail.com>
2016-12-29 11:56:24 +0300

    alive

--- lib/DSL/dsl.ex
+++ lib/DSL/dsl.ex
@@ -2,19 +2,22 @@ defmodule ExSelenidePHP.DSL do
   require IEx
 
   def open(url) do
-      "Opening #{url}"
+      "$this->selenide()->open('#{url}');"
   end
 
   defmacro test(name, do: action) do
       quote do
-        """
+        IO.puts """
         <?php
+
+        use Selenide\\By;
+        use Selenide\\Condition;
 
         class #{unquote(name)}
         {
             function test#{unquote(name)}
             {
-              #{unquote(action)}
+                #{unquote(action)}
             }
         }
         """
